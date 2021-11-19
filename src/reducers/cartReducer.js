b2c0f607fed773ac5dc6d659d4bb19e2679fef49
diarih <diarihza@gmail.com>
2021-11-19 23:42:55 +0700

    done

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,56 +1,158 @@
-import Item1 from '../images/avocado.jpg'
-import Item2 from '../images/carrot.jpg'
-import Item3 from '../images/corn.jpg'
-import Item4 from '../images/garlic.jpg'
-import Item5 from '../images/red-chili.jpg'
-import Item6 from '../images/tomato.jpg'
-import { ADD_TO_CART, REMOVE_ITEM, SUB_QUANTITY, ADD_QUANTITY, ADD_SHIPPING, SUB_SHIPPING } from '../actions/action-types/cartActions'
-
+import Item1 from "../images/avocado.jpg";
+import Item2 from "../images/carrot.jpg";
+import Item3 from "../images/corn.jpg";
+import Item4 from "../images/garlic.jpg";
+import Item5 from "../images/red-chili.jpg";
+import Item6 from "../images/tomato.jpg";
+import {
+  ADD_TO_CART,
+  REMOVE_ITEM,
+  SUB_QUANTITY,
+  ADD_QUANTITY,
+  ADD_SHIPPING,
+  SUB_SHIPPING,
+} from "../actions/action-types/cartActions";
 
 const initState = {
   items: [
-    { id: 1, title: 'Avocado', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 110, img: Item1 },
-    { id: 2, title: 'Carrot', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 80, img: Item2 },
-    { id: 3, title: 'Corn', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 120, img: Item3 },
-    { id: 4, title: 'Garlic', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 260, img: Item4 },
-    { id: 5, title: 'Red Chili', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 160, img: Item5 },
-    { id: 6, title: 'Tomato', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 90, img: Item6 }
+    {
+      id: 1,
+      title: "Avocado",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",
+      price: 110,
+      img: Item1,
+    },
+    {
+      id: 2,
+      title: "Carrot",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",
+      price: 80,
+      img: Item2,
+    },
+    {
+      id: 3,
+      title: "Corn",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",
+      price: 120,
+      img: Item3,
+    },
+    {
+      id: 4,
+      title: "Garlic",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",
+      price: 260,
+      img: Item4,
+    },
+    {
+      id: 5,
+      title: "Red Chili",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",
+      price: 160,
+      img: Item5,
+    },
+    {
+      id: 6,
+      title: "Tomato",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",
+      price: 90,
+      img: Item6,
+    },
   ],
   addedItems: [],
-  total: 0
-
-}
+  total: 0,
+};
 const cartReducer = (state = initState, action) => {
-
   //INSIDE HOME COMPONENT
   if (action.type === ADD_TO_CART) {
     // Add code here
-    return [...state.addedItems, action.payload]
+    let add = state.items.find((e) => e.id === action.id);
+    console.log(add);
+    let find = state.addedItems.find((e) => e.id === action.id);
+    console.log(find);
+
+    if (find) {
+      add.quantity += 1;
+      return {
+        ...state,
+        total: state.total + add.price,
+      };
+    } else {
+      add["quantity"] = 1;
+      return {
+        ...state,
+        addedItems: [...state.addedItems, add],
+        total: state.total + add.price,
+      };
+    }
   }
   if (action.type === REMOVE_ITEM) {
     // Add code here
-    
+    let remItem = state.addedItems.find((e) => e.id === action.id);
+    let item = state.addedItems.filter((e) => e.id !== action.id);
+
+    let total = state.total - remItem.price * remItem.quantity;
+
+    return {
+      ...state,
+      addedItems: item,
+      total: total,
+    };
   }
   //INSIDE CART COMPONENT
   if (action.type === ADD_QUANTITY) {
     // Add code here
+    const add = state.items.find((e) => e.id === action.id);
+    add.quantity += 1;
+    const total = state.total + add.price;
+
+    return {
+      ...state,
+      total: total,
+    };
   }
   if (action.type === SUB_QUANTITY) {
     // Add code here
+    const item = state.items.find((e) => e.id === action.id);
+
+    if (item.quantity <= 1) {
+      let remItem = state.addedItems.find((e) => e.id === action.id);
+      let item = state.addedItems.filter((e) => e.id !== action.id);
+
+      const total = state.total - remItem.price;
+
+      return {
+        ...state,
+        addedItems: item,
+        total: total,
+      };
+    }
+
+    item.quantity -= 1;
+    const total = state.total + item.price;
+
+    return {
+      ...state,
+      total: total,
+    };
   }
 
   if (action.type === ADD_SHIPPING) {
     // Add code here (OPTIONAL)
+    return {
+      ...state,
+      total: state.total + 6,
+    };
   }
 
   if (action.type === SUB_SHIPPING) {
     // Add code here (OPTIONAL)
+    return {
+      ...state,
+      total: state.total - 6,
+    };
+  } else {
+    return state;
   }
+};
 
-  else {
-    return state
-  }
-
-}
-
-export default cartReducer
+export default cartReducer;
